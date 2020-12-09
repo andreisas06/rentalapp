0c8cde9bde041e039cf6a4cdeda94b90bd30fbb4
Andrei Sas <andreisas007@gmail.com>
2020-12-09 16:45:17 +0200

    improvements

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -4,35 +4,38 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Rental App'),
-        elevation: 2,
-      ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                height: 100,
-              ),
-            ),
-            Container(
-              color: Colors.red,
-              child: SizedBox(
-                height: 100,
-              ),
-            ),
-            Container(
-              color: Colors.purple,
-              child: SizedBox(
-                height: 100,
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Rental App'),
+          elevation: 2,
         ),
+        body: _buildContent());
+  }
+
+  Widget _buildContent() {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+        ],
       ),
     );
   }
