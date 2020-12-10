a77e4df6027e2767b25b26a6b0e22db6d64ffba2
Andrei Sas <andreisas007@gmail.com>
2020-12-10 14:10:45 +0200

    creating custom button

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -4,36 +4,45 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Rental App'),
-          elevation: 2,
-        ),
-        body: _buildContent());
+      appBar: AppBar(
+        title: Text('Rental App'),
+        elevation: 2,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
   }
 
   Widget _buildContent() {
-    return Container(
-      color: Colors.yellow,
+    return Padding(
+      padding: EdgeInsets.all(16.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            color: Colors.orange,
-            child: SizedBox(
-              height: 100,
-            ),
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
           ),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              height: 100,
-            ),
+          SizedBox(
+            height: 8,
           ),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              height: 100,
+          RaisedButton(
+            child: Text(
+              'Sign In with Google',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 15,
+              ),
             ),
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+            ),
+            onPressed: () {},
           ),
         ],
       ),
