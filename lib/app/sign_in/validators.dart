1d021bcbecedab4070037b4c0bde1836c798f0b0
Andrei Sas <andreisas007@gmail.com>
2020-12-24 13:05:34 +0200

    adding string validator class

---
+++ lib/app/sign_in/validators.dart
@@ -0,0 +1,11 @@
+abstract class StringValidator {
+  bool isValid(String value);
+}
+
+class NonEmptyStringValidator implements StringValidator {
+  @override
+  bool isValid(String value) {
+    // TODO: implement isValid
+    return value.isNotEmpty;
+  }
+}
