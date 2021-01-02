784997838101e9c0b2981d72ff6d3fc0ab15084c
Andrei Sas <andreisas007@gmail.com>
2021-01-02 15:25:27 +0200

    Creating an AuthProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rental/app/landing_page.dart';
 import 'package:rental/services/auth.dart';
+import 'package:rental/services/auth_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,11 +13,14 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rental App',
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: LandingPage(
-        auth: Auth(),
+    return AuthProvider(
+      auth: Auth(),
+          child: MaterialApp(
+        title: 'Rental App',
+        theme: ThemeData(primarySwatch: Colors.indigo),
+        home: LandingPage(
+          auth: Auth(),
+        ),
       ),
     );
   }
